1fad2c0f2db9be9e54c53c7d769a88d7d4467f1a
Kristine Perry <kristinesperry@Kristines-MacBook-Pro.local>
2019-08-30 22:55:02 -0400

    Profile view created

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,11 +1,13 @@
 // JavaScript Document
 $(function () {
   $(document).scroll(function () {
-    var $nav = $("header.template-1");
-    $nav.toggleClass('scroll bg-dark', $(this).scrollTop() > $nav.height());
+    var $nav = $("header");
+    var $navTemplate1 = $("header.template-1");
+    $nav.toggleClass('scroll', $(this).scrollTop() > $nav.height());
+    $navTemplate1.toggleClass('bg-dark', $(this).scrollTop() > $nav.height());
   });
 });
 
 $( ".navbar-toggler" ).click(function() {
-  $( "header" ).addClass('bg-dark');
+  $( "header.template-1" ).addClass('bg-dark');
 });
